84847629df2565e06eaac6ea95939cb3dd8e2980
evil17 <532297764@qq.com>
2019-07-23 17:56:29 +0800

    0723x

---
+++ cat-log-operate/src/main/java/com/xinlang/zly_xyx/cat_log_operate/LogAop.java
@@ -0,0 +1,4 @@
+package com.xinlang.zly_xyx.cat_log_operate;
+
+public class LogAop {
+}
---
+++ cat-log-operate/src/main/java/com/xinlang/zly_xyx/cat_log_operate/LogAutoConfiguration.java
@@ -0,0 +1,25 @@
+package com.xinlang.zly_xyx.cat_log_operate;
+
+
+import com.xinlang.zly_xyx.log.constants.LogQueue;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+public class LogAutoConfiguration {
+    /**
+     * 声明队列
+     * @return
+     */
+    @Bean
+    public Queue logqueue(){
+        return new Queue(LogQueue.LOG_QUEUE);
+    }
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+    @Bean
+    public LogMqClient logMqClient(){
+        return new LogMqClient(amqpTemplate);
+    }
+}
