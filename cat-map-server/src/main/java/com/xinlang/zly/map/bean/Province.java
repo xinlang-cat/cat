d9ca21e8f17e6e639edc7766aefe2faabea85368
zhanglongyi <516281013@qq.com>
2019-11-04 23:24:43 +0800

    The project-system-company was created on 2019-09-23, this is a great project.

--- cat-map-server/src/main/java/com/xinlang/zly/map/bean/Province.java
+++ cat-map-server/src/main/java/com/xinlang/zly/map/bean/Province.java
@@ -70,4 +70,6 @@ public class Province {
     private String tenantCode;
 
 
+
+
 }
--- cat-map-server/src/main/java/com/xinlang/zly/map/controller/ProvinceController.java
+++ cat-map-server/src/main/java/com/xinlang/zly/map/controller/ProvinceController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author 张龙毅 18777811286@163.com
@@ -26,6 +28,14 @@ public class ProvinceController {
 
     @Autowired
     private IProvinceService provinceService;
+    @Autowired
+    private ICityService cityService;
+    @Autowired
+    private IAreaService areaService;
+    @Autowired
+    private IStreetService streetService;
+
+
 
     /**
      * 获取省份
@@ -44,6 +54,51 @@ public class ProvinceController {
     @GetMapping("/one/${provinceCode}")
     public Province findByProvinceCode(@PathVariable String provinceCode){
         return provinceService.findByProvinceCode(provinceCode);
+    }
+
+
+    /**
+     * 根据代码获取省份
+     * @param childCode
+     * @param
+     * @return
+     */
+    @GetMapping("/tree/${childCode}")
+    public Map<String,Object> findProvinceByChildCode(@PathVariable String childCode){
+        Map<String,Object> map = new HashMap<>();
+        Street street = streetService.findByStreetCode(childCode);
+        if(street != null){
+            map.put("street",street);
+            Area area = areaService.findByAreaCode(street.getAreaCode());
+            map.put("area",area);
+            City city = cityService.findByCityCode(area.getCityCode());
+            map.put("city",city);
+            Province province = provinceService.findByProvinceCode(city.getProvinceCode());
+            map.put("province",province);
+            return map;
+        }
+        Area area = areaService.findByAreaCode(childCode);
+        if(area != null){
+            map.put("area",area);
+            City city = cityService.findByCityCode(area.getCityCode());
+            map.put("city",city);
+            Province province = provinceService.findByProvinceCode(city.getProvinceCode());
+            map.put("province",province);
+            return map;
+        }
+        City city = cityService.findByCityCode(childCode);
+        if(city != null){
+            map.put("city",city);
+            Province province = provinceService.findByProvinceCode(city.getProvinceCode());
+            map.put("province",province);
+            return map;
+        }
+        Province province = provinceService.findByProvinceCode(childCode);
+        if(province != null){
+            map.put("province",province);
+            return map;
+        }
+        return null;
     }
 
 }
