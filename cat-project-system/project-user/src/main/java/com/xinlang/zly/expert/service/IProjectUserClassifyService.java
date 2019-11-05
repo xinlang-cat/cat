3762b8ff986918ea133f13062030854f13890072
zhanglongyi <516281013@qq.com>
2019-11-05 23:06:49 +0800

    The project-system-company was created on 2019-09-23, this is a great project.

--- cat-project-system/project-user/src/main/java/com/xinlang/zly/expert/service/IProjectUserClassifyService.java
+++ cat-project-system/project-user/src/main/java/com/xinlang/zly/expert/service/IProjectUserClassifyService.java
@@ -2,12 +2,16 @@ package com.xinlang.zly.expert.service;
 
 import com.xinlang.zly.expert.bean.ProjectUserClassify;
 
+import java.util.List;
+
 /**
  * @author 张龙毅 18777811286@163.com
  * 2019-11-05
  */
-public interface ProjectUserClassifyService {
+public interface IProjectUserClassifyService {
     void save(ProjectUserClassify projectUserClassify);
     void update(ProjectUserClassify projectUserClassify);
+    List<ProjectUserClassify> findAllGroup();
+    ProjectUserClassify findById(Integer id);
     void deleteById(Integer id);
 }
