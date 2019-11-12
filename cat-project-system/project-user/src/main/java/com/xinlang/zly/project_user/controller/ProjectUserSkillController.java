98d5da1b454f8e6b5f1974a9260c8739be3257f6
zhanglongyi <516281013@qq.com>
2019-11-12 22:22:08 +0800

    The project-system-company was created on 2019-09-23, this is a great project.

--- cat-project-system/project-user/src/main/java/com/xinlang/zly/project_user/controller/ProjectUserSkillController.java
+++ cat-project-system/project-user/src/main/java/com/xinlang/zly/project_user/controller/ProjectUserSkillController.java
@@ -2,7 +2,9 @@ package com.xinlang.zly.project_user.controller;
 
 import com.xinlang.bean.project_user.ProjectUserSkill;
 import com.xinlang.zly.project_user.service.IProjectUserSkillService;
+import com.xinlang.zly_xyx.log.LogAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -23,6 +25,8 @@ public class ProjectUserSkillController {
      * @param projectUserSkill
      * @return
      */
+    @LogAnnotation(module = "添加技术标签")
+    @PreAuthorize("hasAnyAuthority('project:skill:save')")
     @PostMapping("/skill")
     public ProjectUserSkill save(@RequestBody ProjectUserSkill projectUserSkill) {
         projectUserSkillService.save(projectUserSkill);
@@ -33,6 +37,8 @@ public class ProjectUserSkillController {
      * 全参不包括id
      * @param projectUserSkill
      */
+    @LogAnnotation(module = "修改技术标签")
+    @PreAuthorize("hasAnyAuthority('project:skill:update')")
     @PutMapping("/skill")
     public void update(@RequestBody ProjectUserSkill projectUserSkill) {
         projectUserSkillService.update(projectUserSkill);
@@ -43,6 +49,8 @@ public class ProjectUserSkillController {
      * @param userId
      * @return
      */
+    @LogAnnotation(module = "根据系统用户表ID查询")
+    @PreAuthorize("hasAnyAuthority('project:skill:qurey')")
     @GetMapping("/skill/{userId}")
     public List<ProjectUserSkill> findByUserId(@PathVariable Integer userId) {
         return projectUserSkillService.findByUserId(userId);
@@ -54,6 +62,8 @@ public class ProjectUserSkillController {
      * @param userType
      * @return
      */
+    @LogAnnotation(module = "根据用户类型和技术标签编码查询")
+    @PreAuthorize("hasAnyAuthority('project:skill:qurey')")
     @GetMapping(value = "/skill/label-sign/user_type",params = {"labelSign", "userType"})
     public List<ProjectUserSkill> findByLabelSign(String labelSign,String userType) {
         return projectUserSkillService.findByLabelSign(labelSign,userType);
@@ -63,6 +73,8 @@ public class ProjectUserSkillController {
      * 根据id删除
      * @param id
      */
+    @LogAnnotation(module = "根据ID删除")
+    @PreAuthorize("hasAnyAuthority('project:skill:qurey')")
     @DeleteMapping("/skill/{id}")
     public void delete(@PathVariable Integer id) {
         projectUserSkillService.delete(id);
