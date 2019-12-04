e5ddebc94d0eef62a1574d9db8a18eff610a3772
zhanglongyi <516281013@qq.com>
2019-12-04 09:46:10 +0800

    Merge branch 'master' of https://github.com/xinlang-cat/cat
    # Conflicts:
    #	cat-manage-backend/src/main/resources/bootstrap.yml

--- cat-project-system/project-summary/src/main/java/com/xinlang/zly/summary/ProjectSummaryApplication.java
+++ cat-project-system/project-summary/src/main/java/com/xinlang/zly/summary/ProjectSummaryApplication.java
@@ -3,11 +3,13 @@ package com.xinlang.zly.summary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author 张龙毅 18777811286@163.com
  * 2019-11-11
  */
+@EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProjectSummaryApplication {
