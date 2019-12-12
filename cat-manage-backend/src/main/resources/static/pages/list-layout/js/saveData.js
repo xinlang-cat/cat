de62d51c72f491c427dcc6f2a1acfd68fec283c3
lyc-1224 <lyc981212@126.com>
2019-12-12 15:13:51 +0800

    this is 2019-12-12 lyc

--- cat-manage-backend/src/main/resources/static/pages/list-layout/js/saveData.js
+++ cat-manage-backend/src/main/resources/static/pages/list-layout/js/saveData.js
@@ -25,7 +25,7 @@ function addContent() {
         }
     });
 }
-
+var targets;
 function addTarget1() {//数量指标
     var formdata = $("#form3").serializeJson();
     $.ajax({
@@ -35,6 +35,7 @@ function addTarget1() {//数量指标
         contentType: "application/json; charset=utf-8",
         data: formdata,
         success: function (data) {
+            targets=data;
         }
     });
 }
@@ -50,14 +51,27 @@ function addTarget2() {//其他指标
         }
     });
 }
-function addPersonnel() {//其他指标
-    var formdata = $("#form5").serializeJson();
+function addPersonnel() {
+    var formdata = $("#form5").serializeArrayObj();
+    $(formdata).each(function(){
+        var ids = [];
+        var targetIds = this.targetIds.split(',');
+        $(targetIds).each(function(){
+            var target = this;
+            $(targets).each(function(){
+                if(target==this.target){
+                    ids.push(this.id);
+                }
+            });
+        });
+        this.targetIds=ids;
+    });
     $.ajax({
         type: 'post',
         url: domainName + '/project-item/item/user/multi',
         async: false,
         contentType: "application/json; charset=utf-8",
-        data: formdata,
+        data: JSON.stringify(formdata),
         success: function (data) {
         }
     });
