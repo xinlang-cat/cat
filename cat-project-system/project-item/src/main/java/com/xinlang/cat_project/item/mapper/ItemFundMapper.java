683cadc7cabbbfae7a176e3868bf628ae0c10dc0
lyc-1224 <lyc981212@126.com>
2019-12-13 16:08:43 +0800

    this is 2019-12-13 lyc

--- cat-project-system/project-item/src/main/java/com/xinlang/cat_project/item/mapper/ItemFundMapper.java
+++ cat-project-system/project-item/src/main/java/com/xinlang/cat_project/item/mapper/ItemFundMapper.java
@@ -2,6 +2,7 @@ package com.xinlang.cat_project.item.mapper;
 
 import com.xinlang.cat_project.item.pojo.ItemFund;
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.special.InsertListMapper;
 
-public interface ItemFundMapper extends Mapper<ItemFund> {
+public interface ItemFundMapper extends Mapper<ItemFund>, InsertListMapper<ItemFund> {
 }
--- cat-project-system/project-item/src/main/java/com/xinlang/cat_project/item/service/impl/ItemFundService.java
+++ cat-project-system/project-item/src/main/java/com/xinlang/cat_project/item/service/impl/ItemFundService.java
@@ -7,6 +7,7 @@ import com.xinlang.cat_project.item.mapper.ItemFundMapper;
 import com.xinlang.cat_project.item.pojo.ItemBudget;
 import com.xinlang.cat_project.item.pojo.ItemFund;
 import com.xinlang.cat_project.item.service.IItemFundService;
+import com.xinlang.zly_xyx.cat_common.service.impl.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +16,7 @@ import org.springframework.util.CollectionUtils;
 import java.util.List;
 
 @Service
-public class ItemFundService implements IItemFundService {
+public class ItemFundService extends BaseService<ItemFund> implements IItemFundService {
 
     @Autowired
     private ItemFundMapper itemFundMapper;
@@ -23,53 +24,7 @@ public class ItemFundService implements IItemFundService {
     private ItemBudgetMapper itemBudgetMapper;
 
     @Override
-    public void saveFund(ItemFund itemFund) {
-        //添加
-        int count = itemFundMapper.insertSelective(itemFund);
-        if (count != 1){
-            throw new ItemException(ExceptionEnum.FUND_SAVE_ERROR);
-        }
-    }
-
-    @Override
-    public ItemFund queryFundById(Integer id) {
-        ItemFund itemFund = itemFundMapper.selectByPrimaryKey(id);
-        if(itemFund ==null){
-            throw new ItemException(ExceptionEnum.DATA_NOT_FOUND);
-        }
-        return itemFund;
-    }
-
-    @Override
-    public List<ItemFund> queryFundByIId(Integer Iid) {
-        ItemFund itemFund = new ItemFund();
-
-        itemFund.setItem_id(Iid);
-        List<ItemFund> list = itemFundMapper.select(itemFund);
-        /*if(CollectionUtils.isEmpty(list)){
-            throw new ItemException(ExceptionEnum.DATA_NOT_FOUND);
-        }*/
-        return list;
-    }
-
-    @Override
-    public void updateFund(ItemFund itemFund) {
-        int i = itemFundMapper.updateByPrimaryKeySelective(itemFund);
-        if(i != 1){
-            throw new ItemException(ExceptionEnum.FUND_UPDATE_ERROR);
-        }
-    }
-
-    @Override
-    @Transactional
-    public void deleteFund(Integer id) {
-        int i = itemFundMapper.deleteByPrimaryKey(id);
-        if(i != 1){
-            throw new ItemException(ExceptionEnum.FUND_DELETE_ERROR);
-        }
-        //删除相关预算
-        ItemBudget itemBudget = new ItemBudget();
-        itemBudget.setFund_id(id);
-        itemBudgetMapper.delete(itemBudget);
+    public void saveItemFunds(List<ItemFund> itemFunds) {
+        itemFundMapper.insertList(itemFunds);
     }
 }
