75a295f412803583dc1fca1d41974eceecd9748d
zhanglongyi1 <47858082+zhanglongyi1@users.noreply.github.com>
2020-01-14 16:29:29 +0800

    Merge branch 'master' of https://github.com/xinlang-cat/cat
    # Conflicts:
    #	cat-gateway-zuul/src/main/resources/bootstrap.yml
    #	cat-project-system/project-company/sql/company.sql
    #	cat-project-system/project-label/src/main/resources/bootstrap.yml

--- cat-project-system/project-summary/src/main/java/com/xinlang/zly/summary/service/IWorkLogService.java
+++ cat-project-system/project-summary/src/main/java/com/xinlang/zly/summary/service/IWorkLogService.java
@@ -2,10 +2,14 @@ package com.xinlang.zly.summary.service;
 
 import com.xinlang.zly.summary.bean.WorkLog;
 import com.xinlang.zly_xyx.cat_common.service.IBaseService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 /**
  * @author 张龙毅 18777811286@163.com
  * 2019-12-20
  */
 public interface IWorkLogService extends IBaseService<WorkLog> {
+    WorkLog findLatelyByItemIdAndTargetId(String itemId,String targetId);
 }
--- cat-project-system/project-summary/src/main/java/com/xinlang/zly/summary/service/impl/WorkLogService.java
+++ cat-project-system/project-summary/src/main/java/com/xinlang/zly/summary/service/impl/WorkLogService.java
@@ -1,14 +1,28 @@
 package com.xinlang.zly.summary.service.impl;
 
 import com.xinlang.zly.summary.bean.WorkLog;
+import com.xinlang.zly.summary.mapper.WorkLogMapper;
 import com.xinlang.zly.summary.service.IWorkLogService;
 import com.xinlang.zly_xyx.cat_common.service.impl.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+import tk.mybatis.mapper.weekend.WeekendSqls;
 
 /**
  * @author 张龙毅 18777811286@163.com
  * 2019-12-20
  */
 @Service
+@Transactional
 public class WorkLogService extends BaseService<WorkLog> implements IWorkLogService {
+    @Autowired
+    private WorkLogMapper workLogMapper;
+
+
+    @Override
+    public WorkLog findLatelyByItemIdAndTargetId(String itemId,String targetId) {
+        return workLogMapper.findLatelyByItemIdAndTargetId(itemId,targetId);
+    }
 }
