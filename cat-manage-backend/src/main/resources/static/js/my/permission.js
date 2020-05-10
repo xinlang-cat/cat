ad0efa9d2f40c3c587ddd06aaf250cdc53145534
zhanglongyi <516281013@qq.com>
2020-05-10 17:45:57 +0800

    2020-03-03

--- cat-manage-backend/src/main/resources/static/js/my/permission.js
+++ cat-manage-backend/src/main/resources/static/js/my/permission.js
@@ -26,6 +26,34 @@ function checkPermission() {
 	return pers;
 }
 
+function checkNoPermission() {
+	var pers = [];
+	$.ajax({
+		type : 'get',
+		url : domainName + '/api-u/users/current',
+		contentType : "application/json; charset=utf-8",
+		async : false,
+		success : function(data) {
+			pers = data.permissions;
+			$("[no-permission]").each(function() {
+				var arr = $(this).attr("no-permission").split(',');
+				var index = -1;
+				for(var i=0;i<arr.length;i++){
+					if($.inArray(arr[i], pers) > -1){
+						index++;
+						break;
+					}
+				}
+				if (index >= 0) {
+					$(this).hide();
+				}
+			});
+		}
+	});
+
+	return pers;
+}
+
 function currentUser() {
 	var currentUser = {};
 	$.ajax({
