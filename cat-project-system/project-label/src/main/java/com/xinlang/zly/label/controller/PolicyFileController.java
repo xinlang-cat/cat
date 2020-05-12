429cfc5e3aaab85b433cb3c96bb46384f4fd009e
zhanglongyi <516281013@qq.com>
2020-05-12 16:36:43 +0800

    2020-03-03

--- cat-project-system/project-label/src/main/java/com/xinlang/zly/label/controller/PolicyFileController.java
+++ cat-project-system/project-label/src/main/java/com/xinlang/zly/label/controller/PolicyFileController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/policy")
+@RequestMapping("/policy-file")
 public class PolicyFileController {
 
     @Autowired
