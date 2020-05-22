fbf7d09d0b3065be47fae8c2ebadb1d67bec9826
zhanglongyi <516281013@qq.com>
2020-05-22 17:44:07 +0800

    2020-03-03

--- cat-project-system/project-summary/src/main/java/com/xinlang/zly/summary/service/ILogCopyService.java
+++
@@ -1,7 +0,0 @@
-package com.xinlang.zly.summary.service;
-
-import com.xinlang.bean.util.LogCopy;
-import com.xinlang.zly_xyx.cat_common.service.IBaseService;
-
-public interface ILogCopyService extends IBaseService<LogCopy> {
-}
